e87b392258afaf8df4548794f31cfa5c1bcbd58f
Adelmo Gama <gama.a.jose@gmail.com>
2025-08-20 11:15:45 -0300

    Criação do projeto

---
+++ src/main/java/com/estudo/estlist/EstlistApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.estlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudo/estlist/EstlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.estlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
